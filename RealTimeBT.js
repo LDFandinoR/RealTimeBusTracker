467ca0cf53ddb2fd12730e7137ad999b03283f6b
LDFandinoR <laurad.fandino@urosario.edu.co>
2022-05-12 23:17:25 -0500

    map now appears

--- RealTimeBT.js
+++ RealTimeBT.js
@@ -1,10 +1,10 @@
-mapboxgl.accessToken = ''; 
+mapboxgl.accessToken = 'pk.eyJ1IjoibGF1ZmFuZGlub3IiLCJhIjoiY2wydm1uYnFhMGRhczNjcGZzdzNrOXA1eSJ9.Et9DlEQzD6kmGzAcWxydLA'; 
 
 var map = new mapboxgl.Map({
     container: 'map',
-    style: 'mapbpx://styles/mapbox/streets-v11',
+    style: 'mapbox://styles/mapbox/streets-v11',
     center: [-71.104081, 42.365554],
-    zoom: 14
+    zoom: 10
 });
 
 var marker = new mapboxgl.Marker()
@@ -20,12 +20,12 @@ async function run(){
 	console.log(locations);
 
     // timer
-    setTimeout(run, 15000);
+    setTimeout(run, 30000);
 }
-
+accessToken_MBTA = '1e157437-6641-4e9b-b594-ac553e14a244';
 // Request bus data from MBTA
 async function getBusLocations(){
-    const url = '';
+    const url = ;
     const response = await fetch(url);
     const json     = await response.json();
     return json.data;
