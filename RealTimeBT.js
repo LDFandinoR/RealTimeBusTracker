dbc1eff38ecb5df041c064e3b877f1b197485c04
LDFandinoR <laurad.fandino@urosario.edu.co>
2022-05-12 23:22:18 -0500

    fixing url

--- RealTimeBT.js
+++ RealTimeBT.js
@@ -25,7 +25,7 @@ async function run(){
 accessToken_MBTA = '1e157437-6641-4e9b-b594-ac553e14a244';
 // Request bus data from MBTA
 async function getBusLocations(){
-    const url = ;
+    const url = 'https://api-v3.mbta.com/vehicles?filter[route]=1&include=trip';
     const response = await fetch(url);
     const json     = await response.json();
     return json.data;
